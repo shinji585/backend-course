53ac0738a077cf81fb25d63ab7b7a622fdc69b3a
samuel vargas <159861924+shinji585@users.noreply.github.com>
2026-07-05 19:18:20 -0500

    Update app/schemas/tracked/internal.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- app/schemas/tracked/internal.py
+++ app/schemas/tracked/internal.py
@@ -18,5 +18,8 @@ class TrackedProductInternal(TrackedProductBase):
     created_at: Annotated[
         datetime, Field(default_factory=lambda: datetime.now(UTC), description="When tracking started.")
     ]
-    updated_at: Annotated[datetime | None, Field(..., description="Last modification made to the tracked product.")]
+    updated_at: Annotated[
+        datetime | None,
+        Field(default=None, description="Last modification made to the tracked product.")
+    ]
     status: Annotated[Status, Field(description="Describe the status of the tracked product.")]
